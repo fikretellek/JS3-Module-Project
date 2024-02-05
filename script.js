70c21daf66ddb8e43cb1a00795e49b7b8a170b4a
fikretellek <fikretellek@icloud.com>
2024-02-05 16:19:46 +0000

    Created template for episode card

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  createTemplate();
 }
 
 function makePageForEpisodes(episodeList) {
@@ -10,3 +11,24 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
+function createTemplate() {
+  const cardEpisode = document.createElement("template");
+  cardEpisode.setAttribute("class", "cardEpisode");
+
+  const contImage = document.createElement("div");
+  contImage.setAttribute("class", "contImage");
+
+  const imgEpisode = document.createElement("img");
+  imgEpisode.setAttribute("class", "imgEpisode");
+
+  const titleEpisode = document.createElement("h1");
+  titleEpisode.setAttribute("class", "titleEpisode");
+
+  const summaryEpisode = document.createElement("p");
+  summaryEpisode.setAttribute("class", "summaryEpisode");
+
+  contImage.append(imgEpisode);
+  cardEpisode.append(contImage, titleEpisode, summaryEpisode);
+  document.body.append(cardEpisode);
+}
